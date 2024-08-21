2c840bcf951127a85106db475051624a7457f97f
hannahlumanglas <hannahlumanglas07@gmail.com>
2024-08-21 12:01:46 +0800

    Update acp course.py

--- acp course.py
+++ acp course.py
@@ -1,3 +1,3 @@
 print("Welcome to CS : Advance Computer Programming! ")
 name = input("Enter your name: ")
-print("This course will be fun,", name,"!")
+print("This course will be fun,", name + "!")
