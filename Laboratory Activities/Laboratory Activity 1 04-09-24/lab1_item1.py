94d6c728542c3f31bbadd7d0f923997c0b53f4fb
Patreng <hannahlumanglas07@gmail.com>
2024-09-04 12:00:51 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 1 04-09-24/lab1_item1.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the year: ")
+name1 = input("Enter the genre: ")
+name2 = input("Enter the album: ")
+name3 = input("Enter the song title: ")
+name4 = input("Enter the artist: ")
+
+print("--------------------------")
+print("SONG DETAILS")
+print("--------------------------")
+
+print("Year Released: " , num1)
+print("Genre: ", name1)
+print("Album: ", name2)
+print("Title: ","\"" + name3 +"\"")
+print("Artist: ", name4)
