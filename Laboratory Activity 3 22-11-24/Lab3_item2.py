cf790d763869e12517ec5b488fed1707a2f1744b
Patreng <hannahlumanglas07@gmail.com>
2024-11-20 20:09:21 +0800

    Update Lab3_item2.py

--- Laboratory Activity 3 22-11-24/Lab3_item2.py
+++ Laboratory Activity 3 22-11-24/Lab3_item2.py
@@ -1,15 +1,16 @@
 def is_perfect_number(number):
     if number <= 0:
         return False
-    return sum(divisor for divisor in range(1, number) if number % divisor == 0) == number
+    return sum(divisor for divisor in range(1, number) 
+    if number % divisor == 0) == number
 
 while True:
     user_input = input("Enter a number: ")
     try:
         num = int(user_input)
         if is_perfect_number(num):
-            print(f"{num}")
+            print(f"{num} is a perfect number.")
         else:
-            print(f"{num}")
+            print(f"{num} is not a perfect number.")
     except ValueError:
         print("Please enter a valid integer.")
